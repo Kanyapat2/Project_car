e03ccf776361cc35a6b988b0b7066538bb3fb7a4
Your Name <kanyapatjamnong2545@gmail.com>
2024-02-15 06:55:04 +0700

    register+login

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const notFound = require('./middlewares/notFound')
+const errorMiddleware = require('./middlewares/error')
+const authRoute = require('./routes/auth-route')
 
 const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+ app.use('/auth', authRoute)
+
+  app.use( notFound )
+
+  app.use(errorMiddleware)
+
 
 const port = process.env.PORT;
 app.listen(port, () => {
